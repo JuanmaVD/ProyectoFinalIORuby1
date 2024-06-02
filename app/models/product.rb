ab16909c0c250bb8910526977ea0190e9dfa8315
JuanmaVD <JuanmaVD21@gmail.com>
2024-06-02 17:47:08 -0300

    fixfixrelaciones

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,6 @@ class Product < ApplicationRecord
     has_many :product_sales
     has_many :product_purchase_orders
     has_many :purchase_orders, through: :product_purchase_orders
+    has_many :product_sales
+    has_many :sales, through: :product_sales
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,5 @@
 class Sale < ApplicationRecord
     belongs_to :client, optional: true
     has_many :product_sales
+    has_many :products, through: :product_sales
 end
