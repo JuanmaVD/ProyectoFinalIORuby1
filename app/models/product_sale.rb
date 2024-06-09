8b172cf3116cbe7fc6a61dc9e43586b8cb6c2b58
JuanmaVD <JuanmaVD21@gmail.com>
2024-06-09 16:48:53 -0300

    fixcallback

--- app/models/product_sale.rb
+++ app/models/product_sale.rb
@@ -1,4 +1,11 @@
 class ProductSale < ApplicationRecord
     belongs_to :sale
     belongs_to :product
+    after_create :update_product_stock
+
+  private
+def update_product_stock
+    product.update(stock: product.stock - cantVentaProducto)
+end
+
 end
